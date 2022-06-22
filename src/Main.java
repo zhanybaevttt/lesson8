1a2b11e6f3e0ac1dce431a9f2105bcfb84605bd0
програмист <programist@programists-MacBook-Pro.local>
2022-06-22 23:12:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,113 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        int san = 5;
+//        int factorial =1;
+//        for (int i = 1; i <= san; i++){
+//            factorial *= i;
+//
+//        }
+//        System.out.println(factorial);
+        //[] масив
+//        int [] massive ={12,13,14,15,16};
+//        System.out.println(massive[4]);
+//        byte[] byteMassive ={1,22,3,5};
+//        long[] longsArrey={4,5,6,7};
+//        short[] shorttarray ={5,8,9,0,-1,-10};
+//
+//
+//
+//        double[] doublearray = {1.4d , 2.5 , 5.6d};
+//        float[]  farray = {1.9f , 5.6f};
+//        char[] chararray ={'2' ,'5'};
+//        boolean[] barray = {true, false, false,true};
+//        String[] sarray = {"hello","salam"};
+//
+//        int a =1, b =2, c =3;
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+        // int [] array = new int[5]; // [5] = 5 ge barabar {1,2,3,4,5,}
+
+
+//        int [] intMassive = {1,2,3,4,5,6,7};
+//        char[] chrarray = new char[]{'*', '%' , '^'};
+//
+//
+//        int [] imtMassive2 = new int [6];
+//        imtMassive2[0] =1;
+//        imtMassive2[1] =2;
+//        imtMassive2[2] =3;
+//        imtMassive2[3] =4;
+//        imtMassive2[4] =5;
+//        imtMassive2[5] =6;
+
+
+//        System.out.println(intMassive[0]);
+//        System.out.println(chrarray[1]);
+//        System.out.println(imtMassive2[4]);
+
+
+//        System.out.println(intMassive.length);
+//        System.out.println(chrarray.length);
+//        System.out.println(imtMassive2.length);
+
+
+//      boolean[] booleans = new boolean[5];
+//
+//
+//      booleans[0] = true;
+//      booleans[1] = false;
+//      booleans[2] = true;
+//      booleans[3] = false;
+//      booleans[4] = true;
+//
+//        System.out.println(booleans[3]);
+//        System.out.println(booleans[4]);
+
+
+//       int [] intmassive = new int[5];
+//       intmassive[0]=1;
+
+
+//        int[] intarray = {1,2,3,4,5,6,7,8,9};
+//        for (int i = 0; i < intarray.length ;i++){
+//            System.out.println(intarray[i]);
+//        }
+
+
+//        Random random = new Random();
+//        //Scanner scanner = new Scanner(System.in);
+//        int[] intarrey = new int[10];
+//        int count = 0;
+//
+//
+//        for (int i = 0; i < intarrey.length; i++) {
+//            intarrey[i] = random.nextInt(10);
+//
+//
+//        }
+//        for (int i = 0; i < intarrey.length; i++) {
+//            count = count + intarrey[i];
+//        }
+//        System.out.println(count / 10d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
